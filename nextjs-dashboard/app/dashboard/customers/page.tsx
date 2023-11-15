71610768dae3d83c53d9fa6ad11817c6cd011c29
Admin <47unasahmed@gmail.com>
2023-11-15 16:27:50 +0500

    - handles error in customers

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { useEffect, useState } from "react";
+import REact, { useEffect, useState } from "react";
 
 export default function Page() {
-  const [comment, setComment] = useState('')
-  function handleChange(e){
-    setComment(e.target.value)
+  const [comment, setComment] = useState('');
+  function handleChange(e: { target: { value: any; }; }){
+    const updatedcomment = e.target.value
+    setComment(updatedcomment)
   }
 
     return (
@@ -12,7 +13,7 @@ export default function Page() {
         <p>Hello, Alishba </p>
         <form>
           <label>Enter Comment:</label><br />
-          <input value ={comment} onChange={handleChange} />
+          <input value={comment} onChange={handleChange} />
           <h1>{comment}</h1>
         </form>
       </div>
